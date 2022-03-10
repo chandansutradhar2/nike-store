b511c27939fcbe261e14fc31e3a0634cb5e4da33
chandan_naresh <chandansutradhar2@gmail.com>
2022-03-10 11:57:13 +0530

    event handling with arguement / passing parameters

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,23 +2,31 @@ import React, { useState } from "react";
 import "./Header.css";
 
 export function Header() {
-	const navigateTo = (ev) => {
-		alert("clicked event recieved");
-		console.log(ev);
+	const navigateTo = (routeUrl) => {
+		alert(routeUrl + " event recieved");
+		console.log(routeUrl);
 	};
 
 	return (
 		<div style={{ display: "flex", justifyItems: "end", width: "100vw" }}>
 			<ul>
-				<li onClick={navigateTo}>
+				<li onClick={() => navigateTo("nikeglobal.com/troubeticket-center")}>
 					<a>Help</a>
 				</li>
 
-				<li onClick={navigateTo}>
+				<li
+					onClick={() => {
+						navigateTo("nikeclone.com/career");
+					}}
+				>
 					<a>Join Us</a>
 				</li>
 
-				<li onClick={navigateTo}>
+				<li
+					onClick={() => {
+						navigateTo("signin");
+					}}
+				>
 					<a>Sign In</a>
 				</li>
 			</ul>
